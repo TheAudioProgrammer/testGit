e50b9d3f854a71a452dd49bc6fbb9c34c9f8c1ab
Joshua Hodge <joshuahodge@hotmail.com>
2018-01-20 16:33:24 +0000

    adding files

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,104 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public AudioAppComponent
+{
+public:
+    //==============================================================================
+    MainContentComponent()
+    {
+        setSize (800, 600);
+
+        // specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+
+    ~MainContentComponent()
+    {
+        shutdownAudio();
+    }
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+        // This function will be called when the audio device is started, or when
+        // its settings (i.e. sample rate, block size, etc) are changed.
+
+        // You can use this function to initialise any resources you might need,
+        // but be careful - it will be called on the audio thread, not the GUI thread.
+
+        // For more details, see the help for AudioProcessor::prepareToPlay()
+    }
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
+    {
+        
+        //for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+        //{
+           
+            float* const buffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+            float* const buffer2 = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample + 1);
+            
+            for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+            {
+                buffer[sample] = (random.nextFloat() * 2.0f - 1.0f) * 0.25f;
+            }
+            
+            
+        //}
+        
+        
+        
+        
+    }
+
+    void releaseResources() override
+    {
+        // This will be called when the audio device stops, or when it is being
+        // restarted due to a setting change.
+
+        // For more details, see the help for AudioProcessor::releaseResources()
+    }
+
+    //==============================================================================
+    void paint (Graphics& g) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+        // You can add your drawing code here!
+    }
+
+    void resized() override
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+    }
+
+
+private:
+    Random random;
+    //==============================================================================
+
+    // Your private member variables go here...
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+
+// (This function is called by the app startup code to create our main component)
+Component* createMainContentComponent()     { return new MainContentComponent(); }
